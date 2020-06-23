f85d0d5d85fb02f7b6138bd15161b5de01010944
Corn <cmendoza@pivotal.io>
2020-06-22 23:54:40 -0500

    Add data scripts

---
+++ scripts/.Categories.data.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [dbo].[Categories] ON
+INSERT INTO [Categories] ([CategoryID], [CategoryName], [Description]) VALUES (1, N'Cars', NULL)
+INSERT INTO [Categories] ([CategoryID], [CategoryName], [Description]) VALUES (2, N'Planes', NULL)
+INSERT INTO [Categories] ([CategoryID], [CategoryName], [Description]) VALUES (3, N'Trucks', NULL)
+INSERT INTO [Categories] ([CategoryID], [CategoryName], [Description]) VALUES (4, N'Boats', NULL)
+INSERT INTO [Categories] ([CategoryID], [CategoryName], [Description]) VALUES (5, N'Rockets', NULL)
+SET IDENTITY_INSERT [dbo].[Categories] OFF
